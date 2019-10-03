bf7a6652806afb2fb18f11f2fd82b8a051612fa5
zengy <zengcs2018@163.com>
2019-10-03 12:05:04 +0800

    test ci

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,3 @@
 # Hello Luna School
+
+## test travis-ci
---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push git@github.com:lunapen/school.git master
+
